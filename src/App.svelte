3f4ff35b9cf4d72bce457c049ee003a39f4c486d
Bharat Ramnani <bharatramnani94@gmail.com>
2020-05-17 00:29:12 +0530

    HTTPS filter done, basic styles added

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
 	import { onMount } from 'svelte';
 
 	export let entries = [];
+	export let entriesDisplayed = [];
+	export let possibleValuesForHttps = [];
+	export let selectedValueForHttps = null;
 
 	// 	{
 	// 		"API": string;
@@ -16,29 +19,84 @@
 		console.log(`Fetching entries.`);
 		const api = "https://api.publicapis.org/entries";
 		const response = await fetch(api).then(r => r.json());
-		const entries = response ? response.entries : [];
-		console.log(`Fetched ${entries.length} entries.`);
-		return entries;
+		const result = response ? response.entries : [];
+		console.log(`Fetched ${result.length} entries.`);
+		return result;
 	}
 
 	async function fetchAndSetEntries() {
 		entries = await getEntries();
+		entriesDisplayed = [...entries];
 		console.log(`Set entries.`);
 	}
 
-	onMount(fetchAndSetEntries);
+	function getAllPossibleValuesFor(key, entries) {
+		return Array.from(new Set(entries.map(e => e[key])));
+	}
+
+	async function initEverything() {
+		await fetchAndSetEntries();
+		setFilters();
+	}
+
+	function setFilters() {
+		console.log(`Set possibleValuesForHttps to ${possibleValuesForHttps}`)
+		possibleValuesForHttps = getAllPossibleValuesFor("HTTPS", entries);
+	}
+
+	function handlFilterChangeForHttps() {
+		if (selectedValueForHttps === null) {
+			entriesDisplayed = [...entries];
+		} else {
+			entriesDisplayed = entries.filter(e => e["HTTPS"] === selectedValueForHttps);
+		}
+	}
+
+	onMount(initEverything);
 
 </script>
 
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
+</svelte:head>
+
 <main>
+
+	<div class="filters">
+		<form on:submit|preventDefault={handlFilterChangeForHttps}>
+			<p>HTTPS</p>
+			<select bind:value={selectedValueForHttps} on:change={handlFilterChangeForHttps}>
+				<option value={null}>
+					All
+				</option>
+				{#each possibleValuesForHttps as value}
+					<option value={value}>
+						{value}
+					</option>
+				{/each}
+			</select>
+		</form>
+	</div>
+
+
 	<h1>APIs:</h1>
-	<ul>
-		{#each entries as { API, Description, Auth, HTTPS, Cors, Link, Category }, i}
-			<li>
-				{i + 1}: {API}
-			</li>
+	<table>
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>API</th>
+				<th>HTTPS</th>
+			</tr>
+		</thead>
+		{#each entriesDisplayed as { API, Description, Auth, HTTPS, Cors, Link, Category }, i}
+			<tr>
+				<td>{i+1}</td>
+				<td>{API}</td>
+				<td>{HTTPS}</td>
+			</tr>
 		{/each}
-	</ul>
+	</table>
+
 
 </main>
 
